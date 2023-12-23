b1721ce95a330122dad77d859814de1f4795c2b1
Jarod Crawford <jarodcrawford@Jarods-MacBook-Air.local>
2023-12-22 21:40:55 -0600

    Added webpack build

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./client/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/env", {targets: 'defaults'}],["@babel/react"]]
+          },
+        }, //
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'images/[name].[hash:8].[ext]',
+            },
+          },
+        ],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "./index.html",
+      publicPath: '/'
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    client: {
+      overlay: false,
+    },
+  
+    static: {
+      publicPath: "/build",
+      directory: path.join(__dirname, "build"),
+    },
+  },
+};
