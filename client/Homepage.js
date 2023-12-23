eb45f1b61a7b536759a2a25519945ec7b005ec84
Jarod Crawford <jarodcrawford@Jarods-Air.attlocal.net>
2023-12-23 16:12:41 -0600

    Updated server

--- client/Homepage.js
+++ client/Homepage.js
@@ -26,68 +26,69 @@ const MainCard = ({ sx }) => (
 
 const HomePage = () => {
     const [value, setValue] = useState('');
-
+  
     return (
-<Grid container rowSpacing={4.5} columnSpacing={2.75}>
-  <Grid item xs={12} sx={{ mb: -2.25 }}>
-    <Menu />
-  </Grid>
-
-  <Grid item xs={12} md={7} lg={8}>
-    <Grid container alignItems="center" justifyContent="space-between">
-      <Grid item>
-      </Grid>
-      <Grid item>
-        <Stack direction="row" alignItems="center" spacing={0}>
-          <Button
-            size="small"
-            color={'secondary'}
-            variant={'text'}
-          >
-            Button 1
-          </Button>
-          <Button
-            size="small"
-            color={'secondary'}
-            variant={'text'}
-          >
-            Button 2
-          </Button>
-        </Stack>
-      </Grid>
-    </Grid>
-  </Grid>
-
-            <Grid item xs={12} md={5} lg={4} />
-            <Grid item xs={12} md={7} lg={8}>
-                <Grid container alignItems="center" justifyContent="space-between">
-                    <Grid item>
-                        <Typography variant="h5">Test 2</Typography>
-                    </Grid>
-                    <Grid item>
-                        <TextField
-                            id="standard-select-currency"
-                            size="small"
-                            select
-                            value={value}
-                            onChange={(e) => setValue(e.target.value)}
-                            sx={{ '& .MuiInputBase-input': { py: 0.5, fontSize: '0.875rem' } }}
-                        >
-                        </TextField>
-                    </Grid>
-                </Grid>
-                <MainCard sx={{ mt: 1.75 }}>
-                    <Stack spacing={1.5} sx={{ mb: -12 }}>
-                        <Typography variant="h6" color="secondary">
-                            Test 3
-                        </Typography>
-                        <Typography variant="h4">Test 4</Typography>
-                    </Stack>
-
-                </MainCard>
-            </Grid>
+      <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+        <Grid item xs={12} sx={{ mb: -2.25 }}>
+          <Menu />
         </Grid>
+  
+        <Grid item xs={12} md={7} lg={8}>
+          <Grid container alignItems="center" justifyContent="space-between">
+            <Grid item>
+              {/* Content for the first part of the header */}
+            </Grid>
+            <Grid item>
+              <Stack direction="row" alignItems="center" spacing={0}>
+                <Button
+                  size="small"
+                  color={'secondary'}
+                  variant={'text'}
+                >
+                  Button 1
+                </Button>
+                <Button
+                  size="small"
+                  color={'secondary'}
+                  variant={'text'}
+                >
+                  Button 2
+                </Button>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Grid>
+  
+        <Grid item xs={12} md={5} lg={4}>
+        </Grid>
+  
+        <Grid item xs={12} md={7} lg={8}>
+          <Grid container alignItems="center" justifyContent="space-between">
+            <Grid item>
+              <Typography variant="h5">Test 2</Typography>
+            </Grid>
+            <Grid item>
+              <TextField
+                id="standard-select-currency"
+                size="small"
+                select
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                sx={{ '& .MuiInputBase-input': { py: 0.5, fontSize: '0.875rem' } }}
+              />
+            </Grid>
+          </Grid>
+          <MainCard sx={{ mt: 1.75 }}>
+            <Stack spacing={1.5} sx={{ mb: -12 }}>
+              <Typography variant="h6" color="secondary">
+                Test 3
+              </Typography>
+              <Typography variant="h4">Test 4</Typography>
+            </Stack>
+          </MainCard>
+        </Grid>
+      </Grid>
     );
-};
+  };
 
 export default HomePage;
