9a7cf7e0178559bee79c51e00133c6dc6c96a3dc
tanyahang <146495347+tanyahang@users.noreply.github.com>
2024-01-03 16:20:58 -0800

    1st database test pass

--- server/models/databaseModel.js
+++ server/models/databaseModel.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString:
     'postgres://zqoggidn:VeTcCALhagOaX2Rg1mK2kTOuIQE3s6uE@castor.db.elephantsql.com/zqoggidn',
+    // 'postgres://vgpmradm:5cqbNFdn_Zaxkkv2fRy-pEivMMPV6fGl@mahmud.db.elephantsql.com/vgpmradm'
 });
 
 module.exports = {
--- test/database.test.js
+++ test/database.test.js
@@ -10,7 +10,28 @@ describe('Database Model Tests', () => {
 
     afterAll(() => {
         databaseModel.query = originalQuery; // restoring original query method after all tests are done
-    })
-})
+    });
 
-it()
+    it('should retrieve a user by user_id from the database', async () => {
+        const userId = 1;
+        const mockUser = { 
+            user_id: userId, 
+            user_name: 'claire0',
+            password: '1234',
+            google_id: null,
+            email: null,
+            first_name: 'Claire',
+            last_name: 'Huang',
+            phone_number: '(314)-703-6995',
+            is_owner: true 
+        };
+    
+        databaseModel.query.mockResolvedValueOnce({ rows: [mockUser] });
+    
+        const result = await databaseModel.query('SELECT * FROM users WHERE user_id = $1', [userId]);
+    
+        expect(databaseModel.query).toHaveBeenCalledWith('SELECT * FROM users WHERE user_id = $1', [userId]);
+        expect(result.rows).toEqual([mockUser]);
+      });
+
+});
