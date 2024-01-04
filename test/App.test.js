2de11a9f0f2e01300aa8552f13854882263adc0e
Chris Johnson <johnson.chris00@gmail.com>
2024-01-04 12:11:56 -0600

    Added new test for App component

--- test/App.test.js
+++ test/App.test.js
@@ -8,10 +8,16 @@ import App from '../client/App';
 
 describe('Testing App component', () => {
   describe('Test routes in App', () => {
-    test('App should render to screen', async () => {
-      render(<App />);
-      const greeting = await screen.getByRole('heading');
-      expect(greeting).toHaveTextContent('O hai frens!');
+    test('App should render to screen', () => {
+      const { container } = render(<App />);
+      expect(container.firstChild).toHaveClass('App');
+    });
+
+    test('App should render LandingPage component', async () => {
+      const app = render(<App />);
+      console.log(await app.findByText('heading'));
+      // screen.debug();
+      expect(container.secondChild).toHaveClass('LandingPage');
     });
   });
 });
